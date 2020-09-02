a30e1d1996b243cdfdb4c4ae147884d287596b34
どら猫-Doraneko <tranphuquy19@gmail.com>
2020-09-02 16:55:30 +0000

    reload IDE to fix IDE errors

--- src/bt7.cs
+++ src/bt7.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 class MainClass {
@@ -23,28 +24,6 @@ class MainClass {
     int b = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine("UCLN({0}, {1}) = {2}; BCNN({0}, {1}) = {3}", a, b, UCLN(a, b), BCNN(a, b));
-
-    Console.ReadKey();
-  }
-}using System;
-
-class MainClass {
-  // Bài 3 (2nd)
-  public static void Main (string[] args) {
-    Console.Write("Nhập a: ");
-    float a = Convert.ToSingle(Console.ReadLine());
-
-    Console.Write("Nhập b: ");
-    float b = Convert.ToSingle(Console.ReadLine());
-
-    Console.Write("Nhập c: ");
-    float c = Convert.ToSingle(Console.ReadLine());
-
-    bool result = (((a + b) > c) && ((b + c) > a) && ((c + a) > b));
-
-    String _result = result ? "" : "không phải ";
-
-    Console.WriteLine("a = {0}, b = {1}, c = {2} {3}là ba cạnh của tam giác", a, b, c, _result);
 
     Console.ReadKey();
   }
